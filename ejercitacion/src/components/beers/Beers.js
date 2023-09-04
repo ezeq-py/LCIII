f2cbd7b7100123c382a1a384df9486c3bd664d35
Opienso <ezequiel2001uni@gmail.com>
2023-09-04 19:16:08 -0300

    unidad 2 hasta el segundo ejercicio

--- ejercitacion/src/components/beers/Beers.js
+++ ejercitacion/src/components/beers/Beers.js
@@ -1,85 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BeersList from '../beerList/BeersList';
 import BeerAvailable from '../beerAvailable/BeerAvailable';
 import BeerCount from '../beerCount/BeerCount';
 import BeerStyles from '../beerStyles/BeerStyles';
+import ChangeDollar from '../changeDollar/ChangeDollar';
 
 const Beers = () => {
     const beers = [
         {
-        id: 1,
-        beerName: "American",
-        beerStyle: "IPA",
-        price: 3,
-        available: true,
+            id: 1,
+            beerName: "American",
+            beerStyle: "IPA",
+            price: 3,
+            available: true,
         },
         {
-        id: 2,
-        beerName: "Argenta",
-        beerStyle: "IPA",
-        price: 4,
-        available: false,
+            id: 2,
+            beerName: "Argenta",
+            beerStyle: "IPA",
+            price: 4,
+            available: false,
         },
         {
-        id: 3,
-        beerName: "Irish",
-        beerStyle: "Red",
-        price: 4,
-        available: true,
+            id: 3,
+            beerName: "Irish",
+            beerStyle: "Red",
+            price: 4,
+            available: true,
         },
         {
-        id: 4,
-        beerName: "Scotish",
-        beerStyle: "Red",
-        price: 3,
-        available: true,
+            id: 4,
+            beerName: "Scotish",
+            beerStyle: "Red",
+            price: 3,
+            available: true,
         },
         {
-        id: 5,
-        beerName: "DeEssoCiTratta",
-        beerStyle: "APA",
-        price: 3,
-        available: true,
+            id: 5,
+            beerName: "DeEssoCiTratta",
+            beerStyle: "APA",
+            price: 3,
+            available: true,
         },
         {
-        id: 6,
-        beerName: "Santa APA",
-        beerStyle: "APA",
-        price: 3,
-        available: true,
+            id: 6,
+            beerName: "Santa APA",
+            beerStyle: "APA",
+            price: 3,
+            available: true,
         },
         {
-        id: 7,
-        beerName: "German",
-        beerStyle: "Pilsen",
-        price: 1,
-        available: true,
+            id: 7,
+            beerName: "German",
+            beerStyle: "Pilsen",
+            price: 1,
+            available: true,
         },
         {
-        id: 8,
-        beerName: "London Porter",
-        beerStyle: "Porter",
-        price: 2,
-        available: false,
+            id: 8,
+            beerName: "London Porter",
+            beerStyle: "Porter",
+            price: 2,
+            available: false,
         },
         {
-        id: 9,
-        beerName: "Scotish ALE",
-        beerStyle: "Red",
-        price: 5,
-        available: false,
+            id: 9,
+            beerName: "Scotish ALE",
+            beerStyle: "Red",
+            price: 5,
+            available: false,
         },
-        ];
-    const dolar = 731;  //Hago una variable dolar cuando necesito mostrar el precio de los productos en algún componente especifico
-
+    ];
     const beerStyles = beers.map((beer) => beer.beerStyle) //Guardo los estilos de cervezas repetidos en un array aparte que voy a pasar por parametros al componente "BeerStyles"
+    const [newDollar, setNewDollar] = useState(731) //primero declaramos un state y le damos un valor inicial
+    const [showDollar, setShowDollar] = useState(true)
+
+    const setNewDollarHandler = (value) => {  //hacemos una función que contiene la función del useState que cambia el valor de la variable del state
+        setNewDollar(value)
+    }
+    const setShowDollarHandler = (value) => {
+        setShowDollar(value)
+    }
 
     return (
         <div className='beers'>
-            <BeersList beers={beers} dolar={dolar}/>
-            <BeerAvailable beers={beers}/>
-            <BeerCount beers={beers} dolar={dolar}/>
-            <BeerStyles beerStyles={beerStyles}/>
+            <ChangeDollar
+                setNewDollarHandler={setNewDollarHandler}
+                setShowDollarHandler={setShowDollarHandler}
+                showDollar={showDollar}
+            />
+            <BeersList beers={beers} dolar={newDollar} />
+            <BeerAvailable beers={beers} />
+            <BeerCount beers={beers} dolar={newDollar} />
+            <BeerStyles beerStyles={beerStyles} />
         </div>
     )
 }
---
+++ ejercitacion/src/components/changeDollar/ChangeDollar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+import "./changeDollar.css"
+//pasamos la función por props y la ponemos en una función contenedora que va a ir en el evento onChange
+const ChangeDollar = ({ setNewDollarHandler, setShowDollarHandler, showDollar }) => {
+    let toggleClass = showDollar ? "" : "hide"
+
+    const onChangeSetNewDollar = (event) => { //event es un objeto que adentro contiene el valor del evento
+        setNewDollarHandler(event.target.value)
+    }
+    const onClickSetShowDollar = () => {
+        setShowDollarHandler(!showDollar)
+    }
+    return (
+        <div className='dolar-container'>
+            <h1>Ejercicio 2.2</h1>
+            <button onClick={onClickSetShowDollar}>{showDollar ? "Ocultar" : "Mostrar"} componente</button>
+            <div className={`${toggleClass}`}>
+                <fieldset className="dolar">
+                    <legend>Ejercicio 2.1</legend>
+                    <label for="change-dolar">
+                        Ingrese el valor del dolar:
+                        <input id="change-dolar" type='number' onChange={onChangeSetNewDollar} />
+                    </label>
+                </fieldset>
+            </div>
+
+        </div>
+    )
+}
+
+export default ChangeDollar
